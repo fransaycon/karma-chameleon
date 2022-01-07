94dc6ba56683e0fafb772ef46b6a53d401e52c52
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2022-01-07 22:42:59 +0800

    Create stitches configuration

---
+++ src/stitches.js
@@ -0,0 +1,76 @@
+import { createStitches } from "@stitches/react"
+
+const stitches = createStitches({
+  media: {
+    bp1: "(min-width: 320px)",
+    bp2: "(min-width: 768px)",
+    bp3: "(min-width: 1200px)",
+  },
+  theme: {
+    colors: {
+      // generated from coolors.co
+      $aliceBlue: "#F4FAFF",
+      $davyGrey: "#535657",
+
+      $cadetBlue: "#4F646F",
+      $platinum: "#DEE7E7",
+
+      $darkKhaki: "#B5BA72",
+      $darkSlateBlue: "#4F359B",
+
+      // elements
+      $bg: "$aliceBlue",
+      $fg: "$davyGrey",
+    },
+    space: {
+      xxs: "0.422rem",
+      xs: "0.563rem",
+      sm: "0.75rem",
+      rg: "1rem",
+      md: "1.33rem",
+      lg: "1.77rem",
+      xl: "2.369rem",
+      xxl: "3.157rem",
+    },
+    fontSizes: {
+      xxs: "0.422rem",
+      xs: "0.563rem",
+      sm: "0.75rem",
+      rg: "1rem",
+      md: "1.33rem",
+      lg: "1.77rem",
+      xl: "2.369rem",
+      xxl: "3.157rem",
+    },
+    fonts: {
+      abrilFatface: "Abril Fatface, serif",
+      quattrocentroSans: "Quattrocento Sans, sans-serif",
+    },
+  },
+})
+
+const injectGlobalStyles = stitches.globalCss({
+  "*": { boxSizing: "border-box" },
+  "*:after": { boxSizing: "border-box" },
+  "*:before": { boxSizing: "border-box" },
+  body: { margin: 0, padding: 0 },
+  h1: { margin: 0 },
+  "@font-face": [
+    {
+      fontFamily: "Abril Fatface",
+      fontStyle: "normal",
+      fontDisplay: "swap",
+      fontWeight: 400,
+      src: `
+      local('Abril Fatface Regular '),
+      local('Abril Fatface-Regular'),
+      url('/fonts/abril-fatface/abril-fatface-latin-400.woff2') format('woff2'),
+      url('/fonts/abril-fatface/abril-fatface-latin-400.woff') format('woff');
+    `,
+    },
+  ],
+})
+
+injectGlobalStyles()
+
+export default stitches
